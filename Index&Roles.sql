f6342665afa97e36617adda50543220bb7c5d6af
FaynTobio <farisridza01@gmail.com>
2025-12-09 16:35:43 +0800

    Update Index&Roles.sql

--- Index&Roles.sql
+++ Index&Roles.sql
@@ -1,9 +1,7 @@
--- Task 4: 4. Optimization with 
-	-- Discuss the creation of indexes on any columns used frequently in WHERE clauses or as join keys to improve performance.
+-- Discuss the creation of indexes on any columns used frequently in WHERE clauses or as join keys to improve performance.
 
 CREATE INDEX idx_claims_claimdate ON Claims(ClaimDate);
 
---Task 5: Roles and Permissions
 	-- Create roles: ClaimsAnalyst and ClaimsManager.
 	-- ‘ClaimsAnalyst’ role should have read-only access to claims and policies data.
 	-- ‘ClaimsManager’ role should have full access to claims data and the ability to update policy information.
@@ -16,5 +14,6 @@ CREATE ROLE ClaimsManager  LOGIN PASSWORD 'password2';
 
 -- Grant select on necessary tables
 GRANT SELECT ON Claims, Policies, PolicyTypes TO ClaimsAnalyst;
+
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON Claims, Policies, PolicyTypes TO ClaimsManager;
